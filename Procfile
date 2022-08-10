9cac872804603fe6878595e322bc5a6462a988da
Gowtham <gowtham180502@gmail.com>
2022-08-10 19:26:21 +0530

    My Portfolio Website

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: sh setup.sh && streamlit run 00_🎈_Home.py
--- setup.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
