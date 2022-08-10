f2d77fda4dc5c3e8f6d40a4da4b7097bb8e7a0ab
Gowtham <gowtham180502@gmail.com>
2022-08-10 19:15:20 +0530

    My Portfolio Website

--- setup.sh
+++ setup.sh
@@ -4,17 +4,3 @@ echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/secrets.toml
