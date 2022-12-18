6d6fe4193cba9467f9b61609ebf3e120dd5f1808
Gowtham <gowtham180502@gmail.com>
2022-12-18 11:26:49 +0530

    My Portfolio Website

--- pages/01_📚_Projects.py
+++ pages/01_📚_Projects.py
@@ -79,7 +79,7 @@ with six:
         )
     with col2:
         project_info(
-            project_title="Weather forecasting appliction",
+            project_title="Weather forecasting appliction\n",
             demo_link="https://check-weather-in-your-city.herokuapp.com",
             github_link="https://github.com/gowthamr56/weather-application-with-flask.git"
         )
