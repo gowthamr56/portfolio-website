a80e1068a59549f46c505ab7c09240818f574f17
Gowtham <gowtham180502@gmail.com>
2023-02-24 20:22:59 +0530

    Eliminated View button in Resume page

--- pages/03_📜_Resume.py
+++ pages/03_📜_Resume.py
@@ -4,11 +4,11 @@ from generalize import get_page_icon
 import webbrowser
 
 # getting page icon 
-# page_icon = get_page_icon(icon_url="https://raw.githubusercontent.com/gowthamr56/portfolio-website/master/icons/page_icon.png")
+page_icon = get_page_icon(icon_url="https://raw.githubusercontent.com/gowthamr56/portfolio-website/master/icons/page_icon.png")
 
 # page configurations
 st.set_page_config(
-    # page_icon=page_icon,
+    page_icon=page_icon,
     page_title="Gowtham - Resume",
     layout="wide"
 )
@@ -29,12 +29,6 @@ st.markdown(
         <br>
     """, unsafe_allow_html=True
 )
-
-def view_resume(link: str):
-    webbrowser.open(url=link, new=2)
-
-if st.button("View"):
-    view_resume("https://resume.io/r/NkRLMzXwb")
 
 with open("resume_pdf.pdf", "rb") as file:
     st.download_button(f"Click to download", data=file, file_name="Gowtham-Resume.pdf")
