64a42e24310dfe402036367b81181dc9bef2e285
gowthamr56 <gowtham180502@gmail.com>
2023-06-08 13:43:32 +0530

    actions.yml, workflow_logger.py modified

--- workflow_logger.py
+++ workflow_logger.py
@@ -1,5 +1,10 @@
-import datetime
+import datetime, pytz
+
+# IST Time Zone
+tz = pytz.timezone("Asia/Kolkata")
 
 with open('workflow_logs.txt', "a") as file:
-    file.write(datetime.datetime.now().strftime("%d-%m-%y %H:%M") + "\n")
+    utc_time = datetime.datetime.now().strftime("%d-%m-%y %H:%M")
+    ist_time = datetime.datetime.now(tz=tz).strftime("%d-%m-%y %H:%M")
+    file.write(f"{utc_time} | {ist_time}\n")
 
